f66a9eb07173ac46d408b742c1e54f841591a105
pepegov <andelismore@gmail.com>
2023-05-02 12:58:25 +0300

    delete base folder

--- src/MicroserviceTemplate/MicroserviceTemplate.Base/Reposytory/IRepositoryFactory.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MicroserviceTemplate.Base.Reposytory
-{
-    public interface IRepositoryFactory
-    {
-        IRepository<TEntity> GetRepository<TEntity>(bool hasCustomRepository = false) where TEntity : class;
-    }
-}
