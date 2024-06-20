1c96a4bff5a6d0c610b3b56bf71d03d78458605d
pepegov <andelismore@gmail.com>
2024-06-20 16:14:54 +0300

    update template.json

--- src/MicroserviceTemplate/Template.Net.Microservice.BL/IAuditable.cs
+++
@@ -1,8 +0,0 @@
-namespace MicroserviceTemplate.BL;
-
-public interface IAuditable
-{
-    public bool IsDelete { get; set; }
-    public DateTime UpdateAt { get; set; }
-    public DateTime CreateAt { get; set; }
-}
